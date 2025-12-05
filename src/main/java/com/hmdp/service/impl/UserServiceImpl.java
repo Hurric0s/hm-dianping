3075f42047ade9d627b889ec84ceab90b78e9877
m0_74153675 <1978288213@qq.com>
2025-12-05 17:35:36 +0800

    test

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,14 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.entity.User;
 import com.hmdp.exception.PhoneNumberException;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.Random;
 
 /**
  * <p>
@@ -19,13 +22,16 @@ import javax.servlet.http.HttpSession;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
     @Override
     public void sendCode(String phone, HttpSession session) {
-        if(!RegexUtils.isPhoneInvalid(phone)){
-            throw new PhoneNumberException("电话号码格式错误");//
+        if(RegexUtils.isPhoneInvalid(phone)){
+            throw new PhoneNumberException("电话号码格式错误");//验证手机格式
         }
-
+        String code= RandomUtil.randomNumbers(6);
+        session.setAttribute("code",code);
+        log.info("验证码为{}",code);
     }
 }
